bbed100e72cb3321124eb3c15d4b324a5a1ba18e
BaronJake <jake.persons@outlook.com>
2019-10-27 23:45:21 -0500

    adding lucky7

---
+++ Lucky 7/script/test.js
@@ -0,0 +1,38 @@
+function rollDice(numSides) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+function hideTable(){
+  document.getElementById("results").style.display = "none";
+}
+
+function luckySevens(){
+  var bet = document.forms["form"]["bet"].value;
+  document.getElementById("starting").innerHTML = bet;
+  var maxBet = bet;
+  var count = 0;
+  var maxCount = 0;
+  if (bet == "" || isNaN(bet) || bet <= 0){
+    alert("Please enter a valid bet");
+    return false;
+  }
+  while (bet > 0) {
+    dice = rollDice(12);
+    count = count += 1;
+    if (dice == 7){
+      bet = bet + 4;
+    }
+    else{
+      bet = bet - 1;
+    }
+    if (bet > maxBet){
+      maxBet = bet;
+      maxCount = count;
+    }
+  }
+  document.getElementById("rolls").innerHTML= count;
+  document.getElementById("amt").innerHTML = maxBet;
+  document.getElementById("rollHigh").innerHTML = maxCount;
+  document.getElementById("results").style.display = "block";
+  return false;
+}
